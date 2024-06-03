a0fa25327670155652239facfe026a0feccb8e83
Sowmyabala <167969513+Sowmya-21n@users.noreply.github.com>
2024-06-03 14:42:16 +0530

    Add files via upload

---
+++ Question5ArrayIndexExample.java
@@ -0,0 +1,18 @@
+package task11javaexceptionsanderrorhandling;
+public class Question5ArrayIndexExample
+{
+        public static void main(String[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5};
+
+            // Trying to access an index that is out of bounds
+            int index = 10;
+            try
+            {
+                int value = numbers[index];
+                System.out.println("Value at index " + index + ": " + value);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: Array index out of bounds.");
+            }
+        }
+}
---
+++ Question5StringIndexException.java
@@ -0,0 +1,16 @@
+package task11javaexceptionsanderrorhandling;
+public class Question5StringIndexException {
+        public static void main(String[] args)
+        {
+        String text = "Hello, world!";
+
+        // Trying to access a character at an index that is out of bounds
+        int index = 15;
+        try {
+            char character = text.charAt(index);
+            System.out.println("Character at index " + index + ": " + character);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Error: String index out of bounds.");
+        }
+    }
+}
